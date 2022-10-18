910e040259019674d6f966e24242b8005882c723
pormanEdu193 <112736150+pormanEdu193@users.noreply.github.com>
2022-10-17 22:21:58 -0500

    Add files via upload

---
+++ estructuta de datos/lista.cpp
@@ -0,0 +1,79 @@
+#include "lista.h"
+
+lista::lista()
+{
+	setPrimerPtr(0);
+	setUltimoPtr(0);
+}
+
+lista::~lista()
+{
+	;
+}
+
+void lista::setPrimerPtr(nodo* p){
+	primerPtr = p;
+}
+void lista::setUltimoPtr(nodo* u){
+	ultimoPtr = u;
+}
+nodo* lista::getPrimerPtr(){
+	return primerPtr;
+}
+nodo* lista::getUltimoPtr(){
+	return ultimoPtr;
+}
+
+void lista::imprimir(){
+	cout<<"primerPtr = "<<getPrimerPtr()<<endl;
+	cout<<"ultimoPtr = "<<getUltimoPtr()<<endl;
+}
+
+void lista::insertarFinal(producto a){
+	nodo* temp = new nodo(a);
+	if(getUltimoPtr() == 0){
+		setPrimerPtr(temp);
+		setUltimoPtr(temp);
+	}
+	else{
+		ultimoPtr->setSiguientePtr(temp);
+		setUltimoPtr(temp);
+	}
+}
+
+void lista::insertarComienzo(producto b){
+	nodo* temp = new nodo(b);
+	if(getPrimerPtr()==0){
+		setPrimerPtr(temp);
+	}
+	else{
+		temp->setSiguientePtr(primerPtr);
+		setPrimerPtr(temp);
+	}
+}
+
+
+void lista::mostrar(){
+	nodo* aux=getPrimerPtr();
+	if(getPrimerPtr()==0){
+		cout<<"La lista esta vacia"<<endl;
+	}
+	while(aux!=0){
+		aux->getValor().imprimir();
+		//cout<<aux->getSiguientePtr();
+		aux= aux->getSiguientePtr();
+	}
+}
+
+void lista::leer(){
+	ifstream entrada("datosProductos.txt");
+	string n;
+	int p;
+	while(!entrada.eof()){
+		entrada>>n>>p;
+		producto x(n,p);
+		//insertarFinal(x);
+		insertarComienzo(x);
+	}
+}
+
---
+++ estructuta de datos/principal.cpp
@@ -0,0 +1,12 @@
+#include "lista.h"
+
+int main(int argc, char** argv) {
+	nodo x;
+	x.imprimir();
+	lista y;
+	y.leer();
+	y.imprimir();
+	y.mostrar();
+	return 0;
+}
+
